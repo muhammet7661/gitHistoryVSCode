cb4accb64a4b3a4f13de20af6484a9303f317566
ole1986 <ole.k@web.de>
2020-01-07 14:18:10 +0100

    provide repository picker for first use

--- src/types.ts
+++ src/types.ts
@@ -166,6 +166,7 @@ export type CommitComparison = {
 export const IGitServiceFactory = Symbol('IGitServiceFactory');
 
 export interface IGitServiceFactory {
+    repositoryPicker(): Promise<void>;
     createGitService(resource?: Uri | string): Promise<IGitService>;
 }
 
